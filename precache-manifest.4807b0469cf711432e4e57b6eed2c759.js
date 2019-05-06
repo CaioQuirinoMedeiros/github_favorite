9d870e29f666517291c7f71b970fcc4079d3adf7
CaioQuirinoMedeiros <48543208+CaioQuirinoMedeiros@users.noreply.github.com>
2019-05-05 21:32:41 -0300

    Updates

---
+++ precache-manifest.4807b0469cf711432e4e57b6eed2c759.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9442a7a7023649b4f2885b06294c41ef",
+    "url": "/githubfavorite/index.html"
+  },
+  {
+    "revision": "3f3ef7c1233de087a819",
+    "url": "/githubfavorite/static/css/2.a1ff9b9e.chunk.css"
+  },
+  {
+    "revision": "3f3ef7c1233de087a819",
+    "url": "/githubfavorite/static/js/2.5bd80fd4.chunk.js"
+  },
+  {
+    "revision": "255b1cf8201e9219b729",
+    "url": "/githubfavorite/static/js/main.c7d0f000.chunk.js"
+  },
+  {
+    "revision": "9c7b5adc9788c76a69ed",
+    "url": "/githubfavorite/static/js/runtime~main.a3b7e6b2.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/githubfavorite/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/githubfavorite/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/githubfavorite/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/githubfavorite/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/githubfavorite/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
